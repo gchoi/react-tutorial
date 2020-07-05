6ca6b0ef0f6f27bec740feff62d796bb0d1ad438
cinema4dr12 <cinema4dr12@gmail.com>
2020-07-05 13:01:37 +0900

    Tut #04] Setting Attributes - Styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,19 @@ import React, { Component, createElement } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imageUrl: 'https://picsum.photos/200'
-    }
+        count: 0
+    };
+
+    styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    };
 
     render() {
         return (
             <div>
-                <img src={this.state.imageUrl}></img>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
